0addb98a56e5a5765cf77d46b8c9fbba44d2326f
Khirod <khirodbhanja9@gmail.com>
2024-07-20 22:47:12 +0530

    on click menu added to sidebar

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("javascript added");
+console.log("javascript added")
+<div className=" absolute right-3 lg:hidden">
+{menu ? (
+  <IoIosMenu
+    className={`   text-3xl font-semibold `}
+    onClick={() => {
+      setMenu(0);
+      SetDisplay("block");
+    }}
+  />
+) : (
+  <RxCross2
+    className={`  text-3xl font-semibold `}
+    onClick={() => {
+      setMenu(1);
+      SetDisplay("hidden");
+    }}
+  />
+)}
+</div>
