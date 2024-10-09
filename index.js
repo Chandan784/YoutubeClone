d4b1d2a23c9541fb992c9a437e228957562b7ecc
TKBiswalport <biswalt662@gmail.com>
2024-10-10 01:34:34 +0530

    saved changes

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let toggle = document.querySelector("button");
 let sideBar = document.querySelector(".sidebar");
 
 toggle.addEventListener("click", () => {
-    
+    sideBar.classList.toggle("sidebar2");
 });
